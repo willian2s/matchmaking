d1a9aac1b483c71263caa21dadd1a84654e53224
Willian Silva <silva.williansantos@gmail.com>
2021-11-20 13:50:26 -0300

    Initial Commit, create splash screen, and SignIn screen

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+import { styles } from "./styles";
+
+import DiscordImg from "../../assets/discord.png";
+
+interface Props extends TouchableOpacityProps {
+  title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Image, StatusBar, Text, View } from "react-native";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { styles } from "./styles";
+
+import Illustration from "../../assets/illustration.png";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent
+      />
+
+      <Image source={Illustration} style={styles.image} resizeMode="stretch" />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`}e organize suas{`\n`}
+          jogatinas{`\n`}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.8} />
+      </View>
+    </View>
+  );
+}
